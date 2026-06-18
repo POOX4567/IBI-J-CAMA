254ff95c6bb630c3b1785ea9d006dc87ee307874
M.Sc. POOX <127639196+POOX4567@users.noreply.github.com>
2026-06-18 07:41:51 -0600

    Agrega módulo de reportes PDF y actualiza pantallas de empleados (#12)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 import 'screens/login_screen.dart';
 import 'widgets/bottom_nav_bar.dart';
 import 'package:ibi/utils/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Configuración de fechas en español
+  await initializeDateFormatting('es', null);
+
+  // Inicializar notificaciones
   await NotificationService.init();
+
   runApp(const MyApp());
 }
 
@@ -21,8 +29,7 @@ class MyApp extends StatelessWidget {
       home: const LoginScreen(),
       routes: {
         '/login': (context) => const LoginScreen(),
-        '/home': (context) =>
-            const BottomNavBar(), // Ahora el home es el menú inferior
+        '/home': (context) => const BottomNavBar(),
       },
     );
   }
